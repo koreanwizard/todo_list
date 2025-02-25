2dd996b012db5252e7eb35682ce4e5fbf175e522
koreanwizard <harry992178@gmail.com>
2025-02-25 16:06:40 +0900

    tried to add a customized query that are stared

--- src/main/java/com/example/todo/ToDoRepository.java
+++ src/main/java/com/example/todo/ToDoRepository.java
@@ -1,6 +1,14 @@
 package com.example.todo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+
 
 interface ToDoRepository extends JpaRepository<ToDo, Long> {
+
+    @Query("SELECT todo FROM Todo todo WHERE todo.star = true")
+    ToDo findByStarTrue(boolean star);
 }
