e7903a5ed481382e90256e0a9f63016a9967ea94
koreanwizard <harry992178@gmail.com>
2025-02-25 23:12:40 +0900

    tried to create a custom GET request method that returns ToDo.star = true

--- src/main/java/com/example/todo/ToDoController.java
+++ src/main/java/com/example/todo/ToDoController.java
@@ -22,55 +22,68 @@ class ToDoController {
     }
 
 
-    @GetMapping("/todos")
-    CollectionModel<EntityModel<ToDo>> getEveryToDo() {
 
-        List<EntityModel<ToDo>> todos = repository.findAll().stream().map(todo -> EntityModel.of(todo,
-                linkTo(methodOn(ToDoController.class).getSingleToDoByID(todo.getId())).withSelfRel(),
-                linkTo(methodOn(ToDoController.class).getEveryToDo()).withRel("/todos"))).collect(Collectors.toList());
-
-        return CollectionModel.of(todos, linkTo(methodOn(ToDoController.class).getEveryToDo()).withSelfRel());
-    }
-
-
-
-
-    @GetMapping("/todos/{id}")
-    EntityModel<ToDo> getSingleToDoByID(@PathVariable Long id) {
-        /*
-        A generic container EntityModel<T> is provided by Spring HATEOAS.
-        It contains the data and a collection of links.
-
-        Build a link for getSingleToDoByID with a 'self' flag.
-        Build a link for getEveryToDo with a 'todos.'
-         */
-
-        ToDo todo = repository.findById(id).orElseThrow(() -> new ToDoNotFoundException(id));
-
-        System.out.println(todo);
-
-        return EntityModel.of(todo,
-                linkTo(methodOn(ToDoController.class).getSingleToDoByID(id)).withSelfRel(),
-                linkTo(methodOn(ToDoController.class).getEveryToDo()).withRel("todos"));
-    }
 
     @GetMapping("/todos/isStar/{star}")
-    EntityModel<ToDo> getStarToDoByStar(@PathVariable boolean star) {
-        ToDo todo = repository.findByStarTrue(star);
+    List<ToDo> getEveryToDoWithStar(@PathVariable boolean star) {
 
-
-        return EntityModel.of(todo,
-                linkTo(methodOn(ToDoController.class).getStarToDoByStar(star)).withSelfRel());
+        return repository.findByStarTrue(star);
     }
 
 
-//    @GetMapping("/todos/isStar/{star}")
-//    CollectionModel<EntityModel<ToDo>> getEveryToDoWithStar(@PathVariable boolean star) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    @GetMapping("/todos")
+//    CollectionModel<EntityModel<ToDo>> getEveryToDo() {
 //
-//        List<EntityModel<ToDo>> todos = repository.findByStarTrue(star);
+//        List<EntityModel<ToDo>> todos = repository.findAll().stream().map(todo -> EntityModel.of(todo,
+//                linkTo(methodOn(ToDoController.class).getSingleToDoByID(todo.getId())).withSelfRel(),
+//                linkTo(methodOn(ToDoController.class).getEveryToDo()).withRel("/todos"))).collect(Collectors.toList());
 //
-//        return CollectionModel.of(todos, linkTo(methodOn(ToDoController.class).getEveryToDoWithStar(star)).withSelfRel());
+//        return CollectionModel.of(todos, linkTo(methodOn(ToDoController.class).getEveryToDo()).withSelfRel());
 //    }
+//
+//
+//
+//
+//    @GetMapping("/todos/{id}")
+//    EntityModel<ToDo> getSingleToDoByID(@PathVariable Long id) {
+//        /*
+//        A generic container EntityModel<T> is provided by Spring HATEOAS.
+//        It contains the data and a collection of links.
+//
+//        Build a link for getSingleToDoByID with a 'self' flag.
+//        Build a link for getEveryToDo with a 'todos.'
+//         */
+//
+//        ToDo todo = repository.findById(id).orElseThrow(() -> new ToDoNotFoundException(id));
+//
+//        return EntityModel.of(todo,
+//                linkTo(methodOn(ToDoController.class).getSingleToDoByID(id)).withSelfRel(),
+//                linkTo(methodOn(ToDoController.class).getEveryToDo()).withRel("todos"));
+//    }
+//
+
+
+
+
+
+
 
 
 }
