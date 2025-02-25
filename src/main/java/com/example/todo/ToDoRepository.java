43c7fd390c604eb027c695b6b6fba4c8ecfae5b1
koreanwizard <harry992178@gmail.com>
2025-02-25 23:13:58 +0900

    tried to add a custom query where ToDo.star = true

--- src/main/java/com/example/todo/ToDoRepository.java
+++ src/main/java/com/example/todo/ToDoRepository.java
@@ -2,6 +2,8 @@ package com.example.todo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.hateoas.EntityModel;
 
 import java.util.List;
 
@@ -9,6 +11,6 @@ import java.util.List;
 
 interface ToDoRepository extends JpaRepository<ToDo, Long> {
 
-    @Query("SELECT todo FROM Todo todo WHERE todo.star = true")
-    ToDo findByStarTrue(boolean star);
+    @Query(value = "SELECT t FROM Todo t WHERE t.star = true", nativeQuery = true)
+    List<ToDo> findByStarTrue(@Param("star") boolean star);
 }
