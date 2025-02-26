e2d888035a4c10a5684dee00bd2d73cd06a8db69
koreanwizard <harry992178@gmail.com>
2025-02-26 10:56:45 +0900

    deleted some codes that are not used anymore

--- src/main/java/com/example/todo/ToDoRepository.java
+++ src/main/java/com/example/todo/ToDoRepository.java
@@ -1,16 +1,6 @@
 package com.example.todo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.hateoas.EntityModel;
-
-import java.util.List;
-
-
 
 interface ToDoRepository extends JpaRepository<ToDo, Long> {
-
-    @Query(value = "SELECT t FROM Todo t WHERE t.star = true", nativeQuery = true)
-    List<ToDo> findByStarTrue(@Param("star") boolean star);
 }
